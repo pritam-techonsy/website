8724e6a50c725c406c0a568bd078e141bef6d8da
shubham <shubhamborade@gmail.com>
2024-10-20 13:06:28 +0530

    email service in index page, software devlop page ,

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+
+      document.addEventListener("DOMContentLoaded", (event) => {
   gsap.from("#heading", {
     scale: 2,
     duration: 1,
@@ -11,3 +12,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     ease: "bounce.out",
   });
 });
+
+
+function handle_submit(){
+  
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const subject =document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+
+  const templateParams = {
+    from_name : name,
+    from_email : email,
+    from_subject : subject,
+    from_message : message
+  }
+  emailjs.send('service_9tckxf4','template_53bc3u8',templateParams)
+  .then(function(response) {
+    console.log('SUCCESS!', response.status, response.text);
+    alert('Email sent successfully!');
+    window.location.reload();
+}, function(error) {
+    console.log('FAILED...', error);
+    alert('Failed to send email.');
+});
+return false; 
+}
